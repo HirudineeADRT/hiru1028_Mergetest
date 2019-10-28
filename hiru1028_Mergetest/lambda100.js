e985d87d030c4b23f51b8ea7fe67090fb8e66dfe
HirudineeADRT <hirudinee@adroitlogic.com>
2019-10-28 17:55:08 +0530

    changed different places

--- hiru1028_Mergetest/lambda100.js
+++ hiru1028_Mergetest/lambda100.js
@@ -1,14 +1,15 @@
-// successful response indunil 5
+// hirudinee
 let AWS = require('aws-sdk');
+let AWS1 = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 const s3 = new AWS.S3();
+let AWS2 = require('aws-sdk');
 
-const s3 = new AWS.S3();
 // successful response indunil 1
 exports.handler = function (event, context, callback) {
     cognito_idp.listUsers({
         UserPoolId: process.env.UserPoolId_cognitottt,
-        Limit: 10
+        Limit: 100
     }, function (error, data) {
         if (error) {
             // implement error handling logic here
@@ -20,22 +21,22 @@ exports.handler = function (event, context, callback) {
     s3.listObjects({
         'Bucket': 'as2-test-lahiru',
         'MaxKeys': 10,
-        'Prefix': ''
+        'Prefix': 'sample'
     }).promise()
         .then(data => {
-            console.log(data);           
+            console.log(data);
             /*
             data = {
                 Contents: [
                     {
                        ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
                        Key: "example1.jpg",
-                       LastModified: "<Date Representation>",
+                       LastModified: "<Date Representation>",// test
                        Owner: {
                           DisplayName: "myname",
                           ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
                        },
-                       Size: 11,
+                       Size: 1003,
                        StorageClass: "STANDARD"
                     },
                     // {...}
@@ -44,14 +45,14 @@ exports.handler = function (event, context, callback) {
             */
         })
         .catch(err => {
-            console.log(err, err.stack); 
+            console.log(err, err.stack); //this is error stack
         });
     // cmnt 
     // successful response indunil 2
     s3.putObject({
         "Body": "1",
         "Bucket": "aws-amplify-hotspaces",
-        "Key": "10"
+        "Key": "101"
     })
         .promise()
         .then(data => {
@@ -64,16 +65,16 @@ exports.handler = function (event, context, callback) {
             */
         })
         .catch(err => {
-            console.log(err, err.stack); 
+            console.log(err, err.stack);
         });
-        // successful response indunil 3
+    // successful response indunil 3
     s3.copyObject({
         'Bucket': "com.sigma.test.318300609668.google11681355567701687.us-east-1",
         'CopySource': `/com.sigma.test.318300609668.chamath.us-east-1/s`,
         'Key': "s"
     }).promise()
         .then(data => {
-            console.log(data);           
+            console.log(data);
             /*
             data = {
                 CopyObjectResult: {
@@ -85,10 +86,12 @@ exports.handler = function (event, context, callback) {
         })
         // successful response indunil 7
         .catch(err => {
-            console.log(err, err.stack); 
+            console.log(err, err.stack);
         });
-        // successful response indunil 4
+    // successful response indunil 4 fail
 
     callback(null, { "message": "Successfully executed" });
 }
 // successful response indunil 6
+
+//hiru added cmment
